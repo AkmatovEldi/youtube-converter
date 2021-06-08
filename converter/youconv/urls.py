7788f90a9b81eee2fae9cd6211334e842b6b5e8b
AkmatovEldi <akmatoveldi@gmail.com>
2021-06-08 13:24:44 +0600

    create base.html

--- converter/youconv/urls.py
+++ converter/youconv/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.redirect_by_link, name='redirect_by_link'),
-    path('history/', views.get_history, name='get_history'),
-    path('history/<int:history_id>/', views.get_history_details, name='get_history_details'),
+    path('', views.redirect_by_link, name='redirect-by-link'),
+    path('history/', views.get_history, name='get-history'),
+    path('history/<int:history_id>/', views.get_history_details, name='get-history-details'),
 
 ]
 
